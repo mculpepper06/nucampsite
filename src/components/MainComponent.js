40c2eb34f514f983e9b2266b65fd2ece41980c34
Miranda Culpepper <mculpepper@email.arizona.edu>
2020-07-08 18:00:06 -0700

    react functional components fixed (final)

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
-import Header from './HeaderComponent';
-import Footer from './FooterComponent';
 import  { CAMPSITES } from '../shared/campsites';
 
 
@@ -22,11 +20,9 @@ class Main extends Component {
   render() {
       return (
           <div>
-              <Header />
               <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)}/>
               <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]}/>
-              <Footer />
-          </div>
+             </div>
       );
   }
 }
