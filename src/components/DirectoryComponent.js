0f94270a54ada678bd7eca397c4d241ab1c721f9
Miranda Culpepper <mculpepper@email.arizona.edu>
2020-07-08 17:57:45 -0700

    week 3

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-function RenderDirectoryItem({ campsite, onClick }) {
-    return (
+function RenderDirectoryItem({campsite, onClick}){
+    return(
         <Card onClick={() => onClick(campsite.id)}>
             <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-            <CardImgOverlay>
-                <CardTitle>{campsite.name}</CardTitle>
-            </CardImgOverlay>
+                <CardImgOverlay>
+                    <CardTitle>{campsite.name}</CardTitle>
+                </CardImgOverlay>
         </Card>
-
     );
 }
 
-function Directory (props) {
-
-
-        const directory = props.campsites.map(campsite => {
+function Directory(props){
+    
+    const directory = props.campsites.map(campsite => {
             return (
                 <div key={campsite.id} className="col-md-5 m-1">
-                    <RenderDirectoryItem campsite={campsite} onClick={props.onClick}  />  
+                   <RenderDirectoryItem campsite={campsite} onClick={props.onClick} />
                 </div>
             );
         });
@@ -29,10 +27,10 @@ function Directory (props) {
                 <div className="row">
                     {directory}
                 </div>
-
+                
             </div>
         );
-    }
+}
 
 
 export default Directory;
